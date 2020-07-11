a3ee37d1fea3b1051da44cf36859a31411b9a058
Elise Bal <m11elbal@gmail.com>
2020-07-10 23:04:07 -0500

    starting on game object creation

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -4,6 +4,7 @@ import { SafeAreaView, Text, Button } from 'react-native';
 import Fire from '../constants/Fire';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import firebase from 'firebase';
+import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
 
 // type Props = {
 //   navigation: { navigate: (arg0: string) => void, state: {params: {username? : string}} },
@@ -35,6 +36,89 @@ export default function Welcome(props) {
     // console.log(this.state.user)
     console.log('logged out. Did navigation happen?');
   };
+
+  // const [game, gameLoading, gameError] = useDocument(
+  //   firebase.firestore().collection('game').doc('PReC4ht5CcZFvI3cYEnP'),
+  //   {
+  //     snapshotListenOptions: { includeMetadataChanges: true },
+  //   }
+  // );
+
+  // const [userData, userLoading, userError] = useDocument(
+  //   firebase.firestore().collection('users').doc(`${Fire.shared.getUID()}`)
+  // );
+
+  const goToGame = (thing) => {
+    props.navigation.navigate('GameLobby', { theGame: thing });
+  };
+
+  // console.log('game', game.data());
+
+  //visit game Id later
+  const makeNewGame = (newUser, newInput) => {
+    firebase
+      .firestore()
+      .collection('game')
+      .doc()
+      .set(
+        {
+          users: [newUser],
+          currentMeme: '',
+          endMode: false,
+          gameId: '',
+          gameMode: 'regular',
+          gotUsers: false,
+          inputs: [newInput],
+          numUsers: 1,
+          playing: false,
+          winningMeme: '',
+        },
+        { merge: true }
+      )
+      .then((thing2) => console.log('thing2', thing2));
+    // console.log('thing2', thing2);
+    // goToGame(thing2);
+    // return thing2;
+    // add a timestamp to each game object, query the database ordering by the timestamp for the last child
+    //ref.orderByChild('timestamp').limitToLast(1).on('child_added',function(snapshot) {
+    // console.log('new record', snapshot.val());
+    // });
+  };
+  const addUserToGame = () => {
+    firebase
+      .firestore()
+      .collection('game')
+      .where('numUsers', '<', 3)
+      .orderBy('numUsers')
+      .limit(1)
+      .get()
+      .then((query) => {
+        const newUser = { userId: Fire.shared.getUID(), wins: 0, wonMemes: [] };
+        const newInput = { caption: '', userId: Fire.shared.getUID(), vote: 0 };
+        if (query.docs.length) {
+          const thing = query.docs[0];
+          console.log('query', query.docs);
+          let curVal = thing.data().numUsers;
+          let curUsers = thing.data().users;
+          let curInputs = thing.data().inputs;
+          const numOfPlayers = curVal + 1;
+          thing.ref.update({
+            numUsers: numOfPlayers,
+            users: [...curUsers, newUser],
+            inputs: [...curInputs, newInput],
+          });
+          goToGame(thing);
+        } else {
+          makeNewGame(newUser, newInput);
+        }
+      })
+      .catch((err) => {
+        return console.log(err, "Couldn't find it");
+      });
+
+    // goToGame();
+  };
+
   // render(){
   const [user, loading, error] = useAuthState(firebase.auth());
   if (loading) {
@@ -52,10 +136,7 @@ export default function Welcome(props) {
           title={'To User'}
           onPress={() => props.navigation.navigate('UserPages')}
         ></Button>
-        <Button
-          title={'To Game'}
-          onPress={() => props.navigation.navigate('GameLobby')}
-        ></Button>
+        <Button title={'To Game'} onPress={() => addUserToGame()}></Button>
       </SafeAreaView>
     );
   }
